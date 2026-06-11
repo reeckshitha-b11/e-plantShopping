77d438f278b1a49bb139876633273179c502a943
reeckshitha-b11 <reeckshithaa@gmail.com>
2026-06-11 10:01:23 +0530

    Create CartSlice.jsx

---
+++ CartSlice.jsx
@@ -0,0 +1,60 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  items: [],
+  totalQuantity: 0,
+  totalPrice: 0,
+};
+
+const cartSlice = createSlice({
+  name: "cart",
+  initialState,
+  reducers: {
+    addItem: (state, action) => {
+      const newItem = action.payload;
+      const existingItem = state.items.find(
+        (item) => item.id === newItem.id
+      );
+
+      state.totalQuantity++;
+
+      if (!existingItem) {
+        state.items.push({
+          id: newItem.id,
+          name: newItem.name,
+          price: newItem.price,
+          quantity: 1,
+        });
+      } else {
+        existingItem.quantity++;
+      }
+
+      state.totalPrice += newItem.price;
+    },
+
+    removeItem: (state, action) => {
+      const id = action.payload;
+      const existingItem = state.items.find((item) => item.id === id);
+
+      if (!existingItem) return;
+
+      state.totalQuantity--;
+      state.totalPrice -= existingItem.price;
+
+      if (existingItem.quantity === 1) {
+        state.items = state.items.filter((item) => item.id !== id);
+      } else {
+        existingItem.quantity--;
+      }
+    },
+
+    clearCart: (state) => {
+      state.items = [];
+      state.totalQuantity = 0;
+      state.totalPrice = 0;
+    },
+  },
+});
+
+export const { addItem, removeItem, clearCart } = cartSlice.actions;
+export default cartSlice.reducer;
